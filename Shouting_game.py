360a0fde21770881313c73548ad71d27e94731d6
Omoto <c0a2302825@edu.teu.ac.jp>
2024-07-09 18:23:26 +0900

    敵の追加とゲームエリアの設定

--- Shouting_game.py
+++ Shouting_game.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 pg.init()
 
@@ -10,7 +11,13 @@ pg.display.set_caption("しゅぅてぃんぐげぇむ")
 
 # 色の定義
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+
+# ゲームエリア
+GAME_AREA_SIZE = min(SCREEN_WIDTH, SCREEN_HEIGHT) * 0.6
+GAME_AREA_X = (SCREEN_WIDTH - GAME_AREA_SIZE) / 2
+GAME_AREA_Y = (SCREEN_HEIGHT - GAME_AREA_SIZE) / 2
 
 class Player:
     """
@@ -25,10 +32,16 @@ class Player:
 
     def move(self, dx:int, dy:int):
         """
-        自機を速度ベクトルself.x,self.yに基づき移動させる
+        自機を速度ベクトルself.x,self.yに基づき,
+        new_x,new_yとして移動させる
+        プレイヤーの行動範囲を制御する
         """
-        self.x += dx * self.speed
-        self.y += dy * self.speed
+        new_x = self.x + dx * self.speed
+        new_y = self.y + dy * self.speed
+        if (GAME_AREA_X < new_x < GAME_AREA_X + GAME_AREA_SIZE - self.width and
+            GAME_AREA_Y < new_y < GAME_AREA_Y + GAME_AREA_SIZE - self.height):
+            self.x = new_x
+            self.y = new_y
 
     def draw(self, screen: pg.Surface):
         """
@@ -36,8 +49,37 @@ class Player:
         """
         pg.draw.rect(screen, BLUE, (self.x, self.y, self.width, self.height))
 
+
+class Enemy:
+    """
+    敵キャラを表示するクラス
+    """
+    def __init__(self):
+        self.width = 60
+        self.height = 60
+        self.x = random.randint(0, SCREEN_WIDTH - self.width)
+        self.y = 50
+        self.speed = random.uniform(2, 5)
+        self.direction = random.choice([-1, 1])
+
+    def move(self):
+        """
+        敵キャラを速度ベクトルself.x,self.directionに基づき移動させる
+        """
+        self.x += self.speed * self.direction
+        if self.x <= 0 or self.x >= SCREEN_WIDTH - self.width:
+            self.direction *= -1
+
+    def draw(self, screen: pg.Surface):
+        """
+        引数 screen：画面surface
+        """
+        pg.draw.rect(screen, RED, (self.x, self.y, self.width, self.height))
+        
+        
 def main():
     player = Player()
+    enemy = Enemy() # enemy関数の呼び出し
     clock = pg.time.Clock()
 
     running = True
@@ -52,8 +94,14 @@ def main():
         keys = pg.key.get_pressed()
         player.move(keys[pg.K_RIGHT] - keys[pg.K_LEFT], keys[pg.K_DOWN] - keys[pg.K_UP])
 
+        enemy.move()
+        
         screen.fill((0, 0, 0))
+        # プレイヤーの行動範囲を視覚的に表示する    
+        pg.draw.rect(screen, WHITE, (GAME_AREA_X, GAME_AREA_Y, GAME_AREA_SIZE, GAME_AREA_SIZE), 2)
         player.draw(screen)
+        # 敵キャラを表示
+        enemy.draw(screen)
         pg.display.flip()
         clock.tick(60)
 
@@ -63,7 +111,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-#敵の追加とゲームエリアの設定
 #弾の追加と衝突判定
 #HPとSPの追加、敵の動きの改善
 #敵の弾の改善、ゲームオーバー条件の追加
